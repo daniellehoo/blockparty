13b38e4e5e0b749194930f57322ab268fbea748f
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-01 10:04:50 -0400

    updated readme + rendered text

--- src/VRScene.js
+++ src/VRScene.js
@@ -8,18 +8,37 @@ class VRScene extends React.Component {
   render() {
     return (
       <Scene inspector="https://cdn.jsdelivr.net/gh/aframevr/aframe-inspector@master/dist/aframe-inspector.min.js">
+        <a-assets>
+          <img
+            id="groundTexture"
+            src="https://cdn.aframe.io/a-painter/images/floor.jpg"
+          />
+          <img
+            id="skyTexture"
+            src="https://cdn.aframe.io/a-painter/images/sky.jpg"
+          />
+        </a-assets>
         <Entity
+          primitive="a-sky"
+          height="2048"
+          radius="30"
+          src="#skyTexture"
+          theta-length="90"
+          width="2048"
+        />
+
+        {/* <Entity
           geometry={{ primitive: "box" }}
           material={{ color: "red" }}
           position={{ x: 0, y: 0, z: -5 }}
-        />
-        <Entity particle-system={{ preset: "snow" }} />
+        /> */}
+        <Entity particle-system={{ preset: "stars" }} />
         <Entity light={{ type: "point" }} />
         <Entity gltf-model={{ src: "virtualcity.gltf" }} />
         <Entity
-          text={{ value: "Hello, WebVR!" }}
+          text={{ value: "Block Party!", align: "center" }}
+          position={{ x: 0, y: 2, z: -1 }}
           material={{ color: "red" }}
-          position={{ x: 0, y: 2, z: 2 }}
         />
       </Scene>
     );
