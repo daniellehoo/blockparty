1a7045f3bb892c6c00105e133d890865091d1eec
Augusto Corvalan <augustocorvalan@gmail.com>
2020-04-07 22:11:47 -0400

    added floor and sky

--- src/VRScene.js
+++ src/VRScene.js
@@ -9,35 +9,41 @@ class VRScene extends React.Component {
     return (
       <Scene inspector="https://cdn.jsdelivr.net/gh/aframevr/aframe-inspector@master/dist/aframe-inspector.min.js">
         <a-assets>
-          <img
-            id="groundTexture"
-            src="https://cdn.aframe.io/a-painter/images/floor.jpg"
-          />
+          <img src="https://img.gs/bbdkhfbzkk/stretch/https://i.imgur.com/25P1geh.png" id="grid" crossorigin="anonymous"/>
           <img
             id="skyTexture"
-            src="https://cdn.aframe.io/a-painter/images/sky.jpg"
+            src="https://img.gs/bbdkhfbzkk/2048x2048,stretch/http://i.imgur.com/WqlqEkq.jpg"
           />
         </a-assets>
         <Entity
           primitive="a-sky"
           height="2048"
-          radius="30"
+          radius="5000"
           src="#skyTexture"
-          theta-length="90"
+          theta-length="180"
           width="2048"
+          rotation="0 -90 0"
         />
-
-        {/* <Entity
-          geometry={{ primitive: "box" }}
-          material={{ color: "red" }}
-          position={{ x: 0, y: 0, z: -5 }}
-        /> */}
+        <Entity 
+          primitive="a-plane"
+          width="10000"
+          height="10000"
+          rotation="-90 0 0"
+          color="#f600ff"
+          transparent="true"
+          material="src: #grid; repeat: 10000 10000; metalness: 0.6; roughness:0.6; transparent: true;"
+        />
         <Entity
           particle-system={{ preset: "stars" }}
           position={{ x: 0, y: 1, z: -16 }}
         />
-        <Entity light={{ type: "point" }} />
-        <Entity gltf-model={{ src: "virtualcity.gltf" }} />
+        <Entity 
+          primitive="a-light"
+          type="ambient"
+          intensity="1.5"
+          color="#ffaaff"
+          position="0, 2, 5"
+        />
         <Entity
           text={{ value: "Block Party!", align: "center" }}
           position={{ x: 0, y: 2, z: -1 }}
